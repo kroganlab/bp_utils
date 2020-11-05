953dab8d1da8e1f3dc7fc3daf08329a7c648eb15
Ben Polacco <bpolacco@gmail.com>
2020-11-04 16:23:05 -0800

    network propagation files using S_matrix

---
+++ NetworkPropagation.R
@@ -0,0 +1,101 @@
+
+
+LoadNumPyS_matrix <- function (matrixPath, nodesTablePath){
+  library (RcppCNPy)
+  S_matrix <- RcppCNPy::npyLoad(matrixPath)
+  nodeNames <- fread (nodesTablePath)
+  stopifnot (nrow(S_matrix) == nrow(nodeNames))
+  dimnames(S_matrix) <- list (nodeNames$gene_name, nodeNames$gene_name)
+  return(S_matrix)
+}
+
+
+
+# geneHeats : data.table with gene and heat columns
+NetworkPropagateS_matrix <- function(S_matrix, geneHeats,numPermutations = 20000, networkHeatOnly = FALSE, permuteOnlyInObserved=FALSE){
+  message (now(), " Checking S matrix")
+  stopifnot(check_s_matrix(S_matrix))
+  
+  if(not(all(geneHeats$gene %in% rownames(S_matrix)))){
+    numHeats <- nrow(geneHeats)
+    geneHeats <- geneHeats[gene %in% rownames(S_matrix)]
+    message (sprintf("Not all genes with heat are found in network; heats reduced from %d to %d", numHeats, nrow(geneHeats)))
+  }
+  
+  message (now(), " Propagating initial heat")
+  
+  heat.0 <- makeHeat.0(geneHeats, fullGenes = rownames(S_matrix))
+  propagatedHeat <- S_matrix %*% heat.0
+  # confirm conservation of heat: make sure total heat before and after propagation matches
+  stopifnot ( abs(sum(propagatedHeat) - sum(heat.0))  < 0.001)  # allow rounding errors
+  
+  heatToBeat <- propagatedHeat
+  if (networkHeatOnly){
+    heatToBeat <- propagatedHeat - heat.0 * diag(S_matrix)
+  }
+  
+  #permute analysis
+  message (now(), " Building permutation matrix")
+  permutedColumns <- buildPermutedColumns(geneHeats, rownames(S_matrix), numPermutations, limitToObserved = permuteOnlyInObserved)
+  message (now(), " Propagating permutation matrix")
+  permutedHeats <- S_matrix %*% permutedColumns
+  
+  if (networkHeatOnly) permutedHeats <- permutedHeats - permutedColumns * diag(S_matrix)#sweep (permutedColumns, 1, diag(S_matrix), FUN = "*")
+  
+  message (now(), " Summarizing to p-values")
+  # in next two lines, we count (permute-propagate) > 0 and divide by total number of permutes
+  deltaPermutedHeats <- sweep (permutedHeats, 1, heatToBeat) # permute - propagate
+  pValue <- rowSums(deltaPermutedHeats>0)/ncol(deltaPermutedHeats) # count(()>0) /total
+  
+  results <- data.table (gene = rownames(S_matrix),
+                         heat.0 = heat.0,
+                         prop.heat = propagatedHeat[,1],
+                         pvalue = pValue,
+                         adj.pvalue = p.adjust(pValue, method="BH"),
+                         self.heat = heat.0 * diag(S_matrix))
+  
+  return(results)
+}
+
+
+
+now <- function(){
+  format(Sys.time(), "%H:%M:%S")
+} 
+
+check_s_matrix <- function(S_matrix){
+  # cols should sum to 1.  allow rounding error
+  colSums <- colSums(S_matrix)
+  error <- max(colSums-1)
+  return (error < 0.001)
+}
+
+makeHeat.0 <- function (geneHeats, fullGenes){
+  #expand heats to full set in the network, as indicated by names on inv_denom
+  fullHeats <- geneHeats[fullGenes, .(gene, heat), on = "gene"]
+  fullHeats[is.na(heat), heat := 0]
+  heat.0 <- fullHeats$heat
+  names(heat.0) <- fullHeats$gene
+  return(heat.0)
+}
+
+buildPermutedColumns <- function(geneHeats, fullGenes, numPermutations, limitToObserved = FALSE){
+  if(limitToObserved  & (length(unique(geneHeats$heat)) == 1)){
+    message ("limitToObserved = TRUE requires a full set of observed heats, significant and not", 
+             "\n\tgeneHeats currently only has a single heat value",
+             "\n\trunning with limitToObserved is set to FALSE")
+    limitToObserved <- FALSE
+  }
+  if (limitToObserved){
+    heat0 <- makeHeat.0(geneHeats, fullGenes)
+    observedIndex <- match(geneHeats$gene, fullGenes)
+    x0_perm_mat <- do.call(cbind,lapply(1:numPermutations, FUN = function(i){heat0[observedIndex] <- sample(heat0[observedIndex]); return(heat0)}))
+    rownames(x0_perm_mat) <- names(heat0)
+  }else {
+    heat0 <- makeHeat.0(geneHeats, fullGenes)
+    x0_perm_mat <- do.call(cbind,lapply(1:numPermutations, FUN = function(i)sample(heat0)))
+    rownames(x0_perm_mat) <- names(heat0)
+  }
+  return (x0_perm_mat)
+}
+
---
+++ NetworkPropagation_Build_S_matrix.py
@@ -0,0 +1,64 @@
+#! /usr/local/bin/python3
+
+# TODO:  build as an actual script with command line arguments.
+#   as of now, this is just a copy of code run from a command block in RStudio
+
+import networkx as nx
+import numpy as np
+import pandas as pd
+import time
+
+# For math guidance/explanation
+# see https://doi.org/10.1038/nrg.2017.38
+#  Network propagation: a universal amplifier of genetic associations
+# AU  - Cowen, Lenore
+# AU  - Ideker, Trey
+# AU  - Raphael, Benjamin J.
+# AU  - Sharan, Roded
+# PY  - 2017
+# DA  - 2017/09/01
+# TI  - Network propagation: a universal amplifier of genetic associations
+# JO  - Nature Reviews Genetics
+# 
+# This uses an S matrix (terminology from above paper) which is the transpose of 'inv_denom' used elsewhere in Krogan lab
+# to propagate with an S matrix do: S %*% heat_0
+# to propage with inverse_denom do: heat_0 %*% inv_denom * pr
+# Note:
+# ! Order around %*% matters and S includes the pr factor !
+# col sums of S = 1
+# row sums of inv_denom = 1/pr 
+
+
+mouseEdges = pd.read_csv ("~/Downloads//10090.protein.links.v11.0.txt.gz", sep = " ")
+filteredEdges = mouseEdges[mouseEdges['combined_score'] > 600][['protein1','protein2']]
+edge_list = filteredEdges.values.tolist()
+
+# Create Graph
+G = nx.Graph(edge_list)
+G.remove_edges_from(nx.selfloop_edges(G)) # removing selfloops
+largest_cc = max(nx.connected_components(G), key=len) # only taking the largest connected component
+Gc = G.subgraph(largest_cc)
+
+# Calculate matrix inputs
+nodes = Gc.nodes()
+A = nx.adjacency_matrix(Gc, nodelist=nodes)
+D = np.array([x[1] for x in nx.degree(Gc)]) # degree of each node
+
+# integrity check: node order matches
+degreeNodes = np.array([x[0] for x in nx.degree(Gc)])
+assert (all(degreeNodes == nodes))
+
+Dinverse = np.diag(1/D)
+W = np.matmul(A.todense(), Dinverse)
+I = np.identity(W.shape[0])
+
+for pr in (0.2, 0.3):
+  print ("starting matrix inverse calculation at {}".format(time.asctime(time.localtime())))
+  S_matrix = pr * np.linalg.inv(I-(1-pr)*W)
+  print ("finished matrix inverse calculation at {}".format(time.asctime(time.localtime())))
+  np.save("S_matrix.mouse.string.gt600.pr%0.2f.npy"%(pr), S_matrix)
+  # node names
+  nodes_network = pd.DataFrame(nodes) # Getting node names(gene names)
+  nodes_network.rename(columns={0: "gene_name"}, inplace=True) # rename column header
+  nodes_network.to_csv("S_matrix.mouse.string.gt600.pr%0.2f.nodeNames.csv" %(pr), index=False)
+  
