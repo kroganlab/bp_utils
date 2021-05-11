87c214869111c3c9a3513639ad496835104e5686
Ben Polacco <bpolacco@gmail.com>
2021-05-11 15:00:39 -0700

    better error handling

--- LinearModels.R
+++ LinearModels.R
@@ -15,58 +15,91 @@ linearModelsAllProteins <- function (fullDataTable, formulaList, splitColumn = "
   subsetTables <- split (fullDataTable, fullDataTable[[splitColumn]])
   
   # do al linear models, anova, etc. Result is a list of [[anova, coef]] lists
-  coef.anova.list <- lapply (subsetTables, function(sDT){.linearModelsOneProtein(sDT, formulaList)})
+  coef.anova.list <- pbapply::pblapply (subsetTables, function(sDT){.linearModelsOneProtein(sDT, formulaList)})
 
   # get all anova in a  single table
   anovas.list <- lapply(coef.anova.list, `[[`, "anova")
   anovasTable <- rbindlist (anovas.list, idcol = splitColumn, fill = TRUE)
-  setnames(anovasTable, old = c("rn", "Pr(>F)"), new = c("term", "p.value"))
+  setnames(anovasTable, old = c("rn", "Pr(>F)"), new = c("term", "p.value"), skip_absent = TRUE)
   
   # get all coef in a single table
   coefs.list <- lapply(coef.anova.list, `[[`, "coef")
   coefTable <- rbindlist(coefs.list, idcol = splitColumn, fill = TRUE)
   
-  return (list (anova = anovasTable, coef = coefTable))
+  # get all errors and warnings in a single table
+  errors.list <- lapply(coef.anova.list, `[[`, "errWarn")
+  errWarnTable <- rbindlist(errors.list, idcol = splitColumn, fill = TRUE)
+  errWarnTable[err == "NULL", err := NA]
+  errWarnTable[warn == "NULL", warn := NA]
+  
+  return (list (anova = anovasTable, coef = coefTable, errWarn = errWarnTable))
 } 
 
 
 
-
+#' the one-protein linear model computation
 
 .linearModelsOneProtein <- function(subsetDT, formulas ){
-  lms <-tryCatch({
-    lapply (formulas, function(f)lm(f, data = subsetDT))
-  }, error = function(err){
-    # halt execution, but return the error message.
-    print (err)
-    return(err)
-  })
-  if("error" %in% class(lms)){
-    return (data.table(error = as.character(lms)))
-  }  
+  lms.out <- lapply (formulas, .errorWarningCatcherFactory(lm), data = subsetDT )
+
+  errorWarnTable <- data.table (model = names (lms.out),
+                                err = as.character(lapply (lms.out, `[[`, "err")),
+                                warn = as.character(lapply (lms.out, `[[`, "warn")))
+
+  lms <- lapply (lms.out, `[[`, "value")
+  lms <- lms[!sapply(lms, is.null)]
   
-  coef.list <- lapply (lms, function(lm.out)as.data.table(coefficients(summary(lm.out)), keep.rownames = TRUE))
-  coef.table <- rbindlist(coef.list, idcol = "model")
-  setnames (coef.table, old = c("rn", "Pr(>|t|)"), new = c("term", "p.value"))
-  
-  #f.tests from anova function
-  anova.list <- lapply (lms, function(l)as.data.table(anova(l), keep.rownames=TRUE))
-  anovaTables <- rbindlist(anova.list, idcol = "model")
-  
-  anovaTables[, sigCode := dplyr::case_when(`Pr(>F)`< 0.001 ~ "***",
-                                            `Pr(>F)`< 0.01  ~ "**",
-                                            `Pr(>F)`< 0.05  ~ "*",
-                                            `Pr(>F)`< 0.1  ~ ".",
-                                            TRUE ~ "")]
-  return (list (anova = anovaTables[], coef = coef.table))
+  if(length(lms) > 0){
+    coef.list <- lapply (lms, function(lm.out)as.data.table(coefficients(summary(lm.out)), keep.rownames = TRUE))
+    coef.table <- rbindlist(coef.list, idcol = "model")
+    setnames (coef.table, old = c("rn", "Pr(>|t|)"), new = c("term", "p.value"), skip_absent = TRUE)
+    
+    #f.tests from anova function
+    anova.list <- lapply (lms, function(l)as.data.table(anova(l), keep.rownames=TRUE))
+    anovaTables <- rbindlist(anova.list, idcol = "model")
+    
+    anovaTables[, sigCode := dplyr::case_when(`Pr(>F)`< 0.001 ~ "***",
+                                              `Pr(>F)`< 0.01  ~ "**",
+                                              `Pr(>F)`< 0.05  ~ "*",
+                                              `Pr(>F)`< 0.1  ~ ".",
+                                              TRUE ~ "")]
+  } else{
+    anovaTables <- coef.table <-  NULL
+  }
+  return (list (anova = anovaTables, coef = coef.table, errWarn = errorWarnTable))
 }
+
+
+#' see https://stackoverflow.com/questions/4948361/how-do-i-save-warnings-and-errors-as-output-from-a-function/4952908#4952908
+#' takes any function and runs it in an error/warning/message capturing environment. 
+#' Returns a list of value, warnings (including messages) and errors
+#' still can't get it to capture all warnings from anova ...
+.errorWarningCatcherFactory <- function(fun)
+  function(...) {
+    warn <- err <- NULL
+    res <- withCallingHandlers(
+      tryCatch(fun(...), error=function(e) {
+        #cat ("error handled...\n")
+        err <<- conditionMessage(e)
+        NULL
+      }), warning=function(w) {
+        warn <<- append(warn, conditionMessage(w))
+        #cat ("warning handled...\n")
+        invokeRestart("muffleWarning")
+      },message=function(w) {
+        warn <<- append(warn, conditionMessage(w))
+        #cat ("message handled...\n")
+        invokeRestart("muffleMessage")
+      })
+    list(value = res, warn=warn, err=err)
+  }
 
 
 
 #' this function holds a piece of sample code that demonstrate how to use LinearModelsAllProteins
-#' not inteded to be run on its own, you should copy/paste and modify as needed in your own notebook/script
+#' not intended to be run on its own, you should copy/paste and modify as needed in your own notebook/script
 #' 
-#' @param protQuant.txt here this is the output of MSstats::dataProcess()$RunLevelData
+#' @param protQuant.txt the input file here; in this case, the output of MSstats::dataProcess()$RunLevelData
 #' 
 .linearModelUsageExample <- function (protQuant.txt = 
                                       "~/UCSF/kroganlab/BenPolacco/darpa/2021_01_25_Qiongyu_MOR01_PlasmaMembrane_data/2021_01_28_Full_DataProcess_RunlevelData.txt.gz"){
@@ -82,8 +115,7 @@ linearModelsAllProteins <- function (fullDataTable, formulaList, splitColumn = "
   formulas = list (full = LogIntensities~as.factor(time)*ptx + SUBJECT_ORIGINAL,
                    fixed.ptx = LogIntensities~as.factor(time)+ptx + SUBJECT_ORIGINAL,
                    no.subject = LogIntensities~as.factor(time)*ptx )
-  
-  
+
   
   res <- linearModelsAllProteins(protQuant, formulas, "Protein")
   
