54a2154850405bb8beb78a179973d006b09cb947
Ben Polacco <bpolacco@gmail.com>
2023-01-06 17:07:06 -0800

    multiPTM support with labelProteinsMultiPTMs

--- LocatePeptidePtmInProteins.R
+++ LocatePeptidePtmInProteins.R
@@ -1,7 +1,7 @@
 library (data.table)
 # requires stringr, RCurl, seqinr
 
-# next two files originally  in  FragPipe2MSstats.R see
+# next two functions originally  in  FragPipe2MSstats.R see
 # https://kroganucsf.slack.com/archives/CQ50RCEF7/p1629875481043800 
 # (Slack accessible to Krogan Lab members only)
 
@@ -93,15 +93,17 @@ convertMassModificationFormat <- function(specModSequence, mods=c("PH",  "CAM", 
   
   stopifnot (all(names(massFormats) %in% names(artmsFormats))) 
   
+  
   if(!is.null(keepOnly) | removeAll == TRUE){
     mods <- c(keepOnly, "ANY")
   }
   
   # mass of UB is not yet known by me...
+  # do the replacements one by one
   for (mod in mods){
     if (mod %in% names(massFormats)){
       replace <- artmsFormats[[mod]]
-      if (!is.null(keepOnly) && keepOnly != mod)
+      if (!is.null(keepOnly) && !mod %in% keepOnly)
         replace <- ""
       result <- gsub(massFormats[[mod]], replace, result)
     }else (message("I don't yet know how to deal with mod: ", mod))
@@ -213,15 +215,23 @@ downloadUniprotSequences <- function(uniprots, numConnections = 1){
   fastaTxt <- RCurl::getURL(url = fastaURL, perform = numConnections)
   
   cat ("done\n")
-  fasta.dt <- as.data.table(transpose(stringr::str_split(fastaTxt, n =2, pattern = "\n")))    
-  setnames(fasta.dt, new = c("header", "sequence"))
-  fasta.dt <- fasta.dt[grepl ("^>", header),]
-  fasta.dt[, header := gsub("^>", "", header)]
-  fasta.dt[, sequence := gsub ("\n", "", sequence)]
-  fasta.dt[, c("db", "uniprot", "uniprotName") := tstrsplit (header, split = "[ |]", keep = 1:3)]
+  #check for empty strings:
+  fastaTxt <- fastaTxt[fastaTxt != ""]
+  if (length(fastaTxt) > 0){
+    fasta.dt <- as.data.table(transpose(stringr::str_split(fastaTxt, n =2, pattern = "\n")))    
+    setnames(fasta.dt, new = c("header", "sequence"))
+    fasta.dt <- fasta.dt[grepl ("^>", header),]
+    fasta.dt[, header := gsub("^>", "", header)]
+    fasta.dt[, sequence := gsub ("\n", "", sequence)]
+    fasta.dt[, c("db", "uniprot", "uniprotName") := tstrsplit (header, split = "[ |]", keep = 1:3)]
+  }else{
+    fasta.dt <- data.table()
+  }
+  
+  
   numSuccess <- nrow(fasta.dt)
   if (numSuccess < length(uniprots) )
-    message ("Failed to download ", length(uniprots) - numSuccess, " uniprots: ", setdiff(uniprots, fasta.dt$uniprot))
+    message ("Failed to download ", length(uniprots) - numSuccess, " uniprots: ", paste0(setdiff(uniprots, fasta.dt$uniprot), collapse = "; ") )
   return (fasta.dt[])
 }
 
@@ -329,16 +339,31 @@ parenLowerCaseToProteinSiteNames <- function(uniprots, parenLowerCaseFormats, sh
 
 
 
-#' @param proteinNames character vector, each in format like `Q9Y6Y0_SLS[79.9663]FEMQQDELIEKPMSPMQYAR`
+#' @param proteinNames character vector, uniprots or fasta header like `sp|Q04726|TLE3_HUMAN`.
+#'    Also possibly in format like `Q9Y6Y0_SLS[79.9663]FEMQQDELIEKPMSPMQYAR`
+#'    this format may have been an artifact of a processed file I received
+#' @param modPeptideSequence character vector, like SLS[79.9663]FEMQQDELIEKPMSPMQYAR.  
+#'    If NULL, then will look in proteinNames for protein_peptide format
 #' @param ptmType  one of "PH" or in the future "UB"...also KMET1, KMET2, KMET3
 #' @param fastaFile path to the fasta file, uniprot format, that should match the first field in protein names
 #' @param downloadFromWeb logical TRUE = attempt to download uniprots not found in fasta file from web.
 
-fragPipePTMFormat2SiteFormat <- function(proteinNames, ptmType = "PH", fastaFile = NULL, downloadFromWeb = TRUE, proteinPepSep = "_"){
-  mapper <- data.table(fragPipeProteinName = unique(proteinNames))
+fragPipePTMFormat2SiteFormat <- function(proteinNames, 
+                                         modPeptideSequences = NULL, 
+                                         ptmType = "PH", fastaFile = NULL, downloadFromWeb = TRUE, proteinPepSep = "_"){
+  mapper <- unique(data.table (fragPipeProteinName = proteinNames, modPeptide = modPeptideSequences))
   
-  mapper[, c("uniprot", "modPeptide") := tstrsplit(fragPipeProteinName, proteinPepSep)]
+  # handle various proteinNames formats 
+  if (is.null(modPeptideSequences)){
+    mapper[, c("uniprot", "modPeptide") := tstrsplit(fragPipeProteinName, proteinPepSep)]
+  } else{
+    mapper[, uniprot := NA_character_]
+    mapper[grep ("\\|", fragPipeProteinName), uniprot := tstrsplit(fragPipeProteinName, "\\|")[[2]]]
+    mapper[is.na(uniprot), uniprot := fragPipeProteinName]
+  }
   
+  #  ?do anything about contam tags taht might be in `contam_sp|P00761|TRYP_PIG`  ?
+
   if (ptmType %in% c("KMET1", "KMET2", "KMET3")){
     mapper[, modPeptideLC.PTM := convertLysMethylationMassFormat(modPeptide, keepOnly = ptmType)]
   } else{
@@ -412,3 +437,137 @@ fragPipePTMFormat2SiteFormat <- function(proteinNames, ptmType = "PH", fastaFile
   return (mapper.collapsed[proteinNames, , on =  "fragPipeProteinName"]$ptmSiteCombo)
 }
 
+
+
+#' @param msInput, a data.table in format suitable for MSstats input. 
+#'                 expects columns ProteinName (e.g. a uniprot id); PeptideSequence (with modifications)
+#' @param proteinSeqs, a data.table with full length seqeunce information for looking up the peptides in msInput
+#'                expects columns `ProteinName` that matches format of `ProteinName` in `msInput`, and `sequence`
+#'                The function `loadUniprots` is handy for this.
+#'                Alternatively, this can be a path to a fasta file, in which case it will load and use uniprot ID as
+#'                the `ProteinName``
+#' @param ptms, a named character vector of exact PTM strings that are inserted between capital letter amino acids in teh modified peptide string
+#'              Also, beware partial matches!  If one PTM format is a subset of another, this will fail.              
+labelProteinsMultiPTMs <- function(msInput,
+                                   proteinSeqs,
+                                   ptms = c(ph = "[79.9663]",
+                                            r = "[100.0]",
+                                            rp = "[175.0]",
+                                            rpp = "[250.0]")){
+  
+ 
+  ###################################################################
+  # Warn and check about modification format: no capital letters allowed 
+  ####################################################################
+  for (ptm in ptms){
+    if (sum(grepl(ptm, ptms, fixed = TRUE)) > 1){
+      message (ptm, " is a substring of at least one other PTM")
+      stop("PTMs can not be a substring of another PTM")
+    }
+  }
+  
+  peptides <- unique(msInput$PeptideSequence)
+  detectedModifications <- strsplit(peptides, "[A-Z]")  |> # all things between capital letters
+    unlist() |> unique() |>  # remove redundants
+    setdiff ("")  # remove the empty string
+  
+  cat ("Notice: Any capital letter in a peptide sequence is interpreted as a peptide AA. Capital letters in the modification string, even within brackets, will cause this to fail. Detected modifications: \n")
+  cat ("\t",  sprintf ('"%s" ', detectedModifications), "\n")
+  cat ("If any of these seem wrong, it is likely because a capital letter was used. Fix and start over.", "\n")
+  
+  
+  ####################################################
+  # Locate each PTM of interest in its modified peptide
+  # each PTM per peptide gets its own row.
+  
+  # helper function
+  .locatePTMInPeptide <- function (format, peptides){
+    dt <- data.table(modPeptide = peptides)
+    dt[, ptmPositions := stringr::str_locate_all(modPeptide,  stringr::fixed  (format) )]
+    expanded <- dt[lapply(ptmPositions, nrow) > 0,
+                   .(modPepPos = ptmPositions[[1]][,"start"] - 1),  # subtract 1 because the (ph) labels the preceding AA,
+                   by = .(modPeptide)]
+    return (expanded)
+  }
+  
+  modPepPos.ls <- lapply(ptms, .locatePTMInPeptide, peptides)
+  modPepPos <- rbindlist(modPepPos.ls, idcol = "ptm")
+  modPepPos[, modAA := substr(modPeptide, modPepPos, modPepPos)] # this will fail if somehow there are two modifications one after another.  I can't imagine that possibility for now...
+  
+  
+  
+  #######################################
+  # locate peptides in proteins
+  #########################################
+  # do we have a data.table of sequences or a path to a fasta file:
+  if (! "data.frame" %in% class(proteinSeqs)){ 
+    cat  ("Loading seqeunces from fasta at ", proteinSeqs, "\n")
+    proteinSeqs <- loadUniprots(unique(msInput$ProteinName), 
+                                fastaFile = proteinSeqs,
+                                downloadFromWeb = FALSE)
+    proteinSeqs[, ProteinName := uniprot] # to match expectations below
+    
+  }
+
+  # clean modified peptide for lookups
+  modPepPos[, cleanPeptide := gsub ("[^A-Z]", "", modPeptide)]
+  
+  # map ProteinName back into table:
+  modPepPos[unique(msInput[, .(modPeptide = PeptideSequence, ProteinName)]),
+            ProteinName := i.ProteinName,
+            on = "modPeptide"]
+  
+  # locate cleanSeq in parent sequence, but first a couple checks based on how many times each peptide occurs in the protein
+  modPepPos[proteinSeqs, peptideCopies := stringr::str_count(sequence, cleanPeptide),on = "ProteinName"]
+  if (any (modPepPos$peptideCopies == 0)){
+    notMatched <- unique(modPepPos[peptideCopies == 0]$ProteinName)
+    message (length(notMatched), " peptides could not be located within their protein. Is this the correct FASTA?","\n\t")
+    print(head(notMatched))
+    message ("The unmatched peptides will be removed from futher analysis")
+    modPepPos <- modPepPos[peptideCopies > 0]
+  }
+  if (any (modPepPos$peptideCopies > 1)){
+    message ("Some peptides are duplicated within their protein.  In these cases only the first possible location will be labeled")
+    print (modPepPos[peptideCopies > 1 , 
+                     .(maxCopyNumber = max(peptideCopies), countPeptidesWithDups = length(unique(cleanPeptide))),
+                     by = ProteinName])
+  }
+  
+  modPepPos[proteinSeqs, peptideStart := stringr::str_locate(sequence, cleanPeptide)[,"start"],on = "ProteinName" ]
+  
+  
+  # ##############################################
+  # translate modPepPos to cleanPepPos, by counting capital letters in modPeptide. 
+  # the AA counts: 
+  aaCounts <- lapply (strsplit(modPepPos$modPeptide, ""),  # split by each character
+                      function(chars)cumsum(chars %in% LETTERS)) # use cumsum to count TRUEs
+  
+  
+  # use counter variable to 'loop' over each row and map modPepPos to the aaCounts
+  modPepPos[, cleanPepPos := sapply (1:length(aaCounts), function(i) aaCounts[[i]][modPepPos[i]] ) ]
+  
+  # sanity check that we get same AA in modPeptide as cleanPeptide
+  modPepPos[, testModAA := substr(cleanPeptide, cleanPepPos, cleanPepPos)]
+  stopifnot(modPepPos[, all(testModAA == modAA)])
+  modPepPos[, testModAA := NULL]
+  
+  ################################
+  # finally a protein-level position
+  modPepPos[, pos := peptideStart + cleanPepPos - 1 ]
+  
+  
+  ############################
+  # package into names
+  modPepPos[, siteProteinName := paste0(ProteinName, "_", modAA, pos, ptm)]
+  # wrap up to per-peptide names
+  setorder(modPepPos, ProteinName, pos)
+  newProteinNames <- modPepPos[, .(ProteinName = paste0(siteProteinName, collapse = ";")),  by = .(modPeptide)]
+  
+  
+  ###########################3
+  # modify msInput 
+  cat ("Modifying MSstats input file with new PTM-specific 'ProteinName's. Original ProteinName in column og.ProteinName\n")
+  msInput[, og.ProteinName := ProteinName]
+  msInput[newProteinNames, ProteinName := i.ProteinName, on = c(PeptideSequence = "modPeptide")]
+  return (msInput)
+}
