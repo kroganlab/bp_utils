e0299c36b8762a2523c0680979ef50b2e1886861
bpolacco <bpolacco@gmail.com>
2024-01-08 12:54:04 -0800

    pre and post process shell scripts

---
+++ af.template.dir/af.postOnly.sh
@@ -0,0 +1,42 @@
+#!/bin/env bash
+#
+##$ -q gpu.q
+#$ -N af.postProcess
+#$ -cwd
+#$ -j y               # STDERR and STDOUT should be joined
+###$ -l h_rt=24:00:00
+#$ -l h_rt=11:59:00
+#$ -l mem_free=60G
+#$ -l scratch=50G
+##$ -l compute_cap=80,gpu_mem=40G
+
+##$ -t 1-67             ## job array with xx tasks
+
+# if not running with sge task array, set to 5
+taskID=${SGE_TASK_ID:-5}
+
+
+
+#
+# Compute cap for A100 GPU is 8.0 (40 or 80 GB), for A40 GPU is 8.6 (48 GB).
+#
+
+t0=$(date --rfc-3339=seconds)
+
+echo "QUEUE: $QUEUE"
+echo "SGE_GPU: $SGE_GPU"
+export CUDA_VISIBLE_DEVICES=$SGE_GPU
+
+# CHANGE THIS 67 to match your job numbers
+for taskID in {1..1000};do
+
+./AF_saveMSAS.231.py --model_preset=multimer --job_id=$taskID \
+	--master_fasta=masterFasta.fasta \
+	--jobTable=AlphaFoldJobList.csv \
+	--setup_job=False \
+        --run_alpha_fold=False \
+	--postprocess_job=True
+done
+
+t1=$(date --rfc-3339=seconds)
+echo "Duration: $t0 -- $t1"
