244a1517b48be156dab68e2acfcd6121aae97958
Yuan Zhou <alanazhouyuan@gmail.com>
2025-07-31 16:22:26 -0700

    add multiUniprots2multisomething
    can map to ENSEMBL; ENSEMBLPROT; ENSEMBLTRANS

--- UniprotIDMapping.R
+++ UniprotIDMapping.R
@@ -751,7 +751,28 @@ multiUniprotSites2multiGeneSites <- function (uniprotSites, sep = ";", siteSep =
   return(mapper[toGenes,,on="uniprotSites"]$geneSite)
 }
 
-
+multiUniprots2multisomething <- function (uniprots, sep = ";", something = "ENSEMBL", species = "HUMAN", simplify = FALSE, useDatFile = FALSE, allowDups = FALSE){
+  toGenes <- data.table(uniprots = uniprots)
+  toGenes <- toGenes[,.(singleUniprot = unlist(strsplit(uniprots, sep))),by = uniprots]
+  toGenes[,singleGene := translateUniprot2Something(singleUniprot, something = something, species = species, useDatFile = useDatFile)]
+  toGenes <- toGenes[!is.na(singleGene), ]
+  if (simplify == TRUE){
+    simplify = function(x)unique(sort(x))
+  }else if (simplify == FALSE){
+    simplify = identity # do nothing
+  }else if (! "function" %in% class (simplify)){
+    stop("unexpected simplify format")
+  }
+  toGenes <- toGenes[, .(genes = paste(simplify (singleGene), collapse=sep)), by = uniprots]
+  if(!allowDups){
+    duplicatedGeneNames <- unique(toGenes[duplicated(genes)]$genes)
+    toGenes[genes %in% duplicatedGeneNames, genes := paste0(genes, ".", uniprots)]
+  }
+  
+  setkey(toGenes, uniprots)
+  
+  return(toGenes[uniprots, genes])
+}
 
 # gene aliases
 
